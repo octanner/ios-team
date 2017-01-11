eadc4da4cbeb82a83aed07336c164fdd2c90b60a
Jason Larsen <jason.larsen@gmail.com>
2017-01-11 10:32:53 -0700

    add some basic API snippets

---
+++ Snippets/apiaccess.swift
@@ -0,0 +1,22 @@
+---
+title: "Network Access Pattern"
+summary: "Boilerplate for the network access pattern"
+completion-scope: Class Implementation
+---
+
+import Foundation
+import Marshal
+import NetworkStack
+
+protocol <#Type#>NetworkAccess {
+    <#function definitions#>
+}
+
+
+struct <#Type#>NetworkAPIAccess: <#Type>NetworkAccess {
+
+    var request: NetworkRequests = NetworkAPIRequests()
+
+    <#function implemetations#>
+
+}
