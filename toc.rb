22193aadc3b93842b1ef0fff06aa479ed3a11497
Tim Shadel <tim@shadelsoftware.com>
2017-01-27 20:20:52 -0700

    Make TOC script executable

--- toc.rb
+++ toc.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+
+# See https://gist.github.com/asabaylus/3071099#gistcomment-1593627
 doc = IO.readlines(ARGV[0])
 lines = doc.select { |l| l.start_with? "#" }.map do |l|
   indent = l.chomp.gsub(/^# .*/, '* ').gsub(/^## .*/, '    * ').gsub(/^### .*/, '        * ')
